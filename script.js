3d158202eaf3bc88b361f4ee734ea958eb86453d
Oosh74 <usherjared@yahoo.com>
2025-06-06 16:42:43 -0400

    Add light/dark mode toggle

--- script.js
+++ script.js
@@ -63,6 +63,17 @@ const domLogic = (() => {
     return startGame;
   };
 
+  const setTheme = () => {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+
+    root.className = newTheme;
+
+    document.querySelector('.theme-name').textContent = newTheme;
+  };
+
+  document.querySelector('.theme-toggle').addEventListener('click', setTheme);
+
   return { getHelperText, instantiateDomBoard, getStartBtn };
 })();
 
